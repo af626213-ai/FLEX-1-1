f84f1cfc721f639adc6e82b123ea925e25a8caa8
af626213-ai <af.626213@gmail.com>
2026-04-21 20:02:15 +0900

    Update episode2 with new translations and quizzes

--- src/data/episodes/episode2.ts
+++ src/data/episodes/episode2.ts
@@ -5,10 +5,10 @@ export const episode2: Episode = {
   title: "It is Good to be Nervous!",
   script: "Are you often nervous during a test or a presentation? That is not a bad thing. You are nervous because you care about the results. That means that you are serious. You do not want to fail. You want to do well. Be glad that you are worried and change that feeling into something positive. Then, give 120% to achieve your goal!",
   slash_script: "Are you often nervous / during a test / or a presentation? / That is not a bad thing. / You are nervous / because you care / about the results. / That means that / you are serious. / You do not want to fail. / You want to do well. / Be glad / that you are worried / and change that feeling / into something positive. / Then, give 120% / to achieve your goal!",
-  japanese_translation: "テストや発表の最中、よく緊張しますか？それは悪いことではありません。あなたが緊張するのは、結果を気にしているからです。それはあなたが真剣だということを意味します。失敗したくない、うまくいってほしいと思っているのです。悩んでいることを喜び、その感情をポジティブなものに変えましょう。そして、目標を達成するために120％の力を出し切りましょう！",
+  japanese_translation: "あなたはよく緊張しますか？ / テストの間に / あるいはプレゼンテーション（の間に）？ / それは悪いことではありません。 / あなたが緊張するのは / ～だからです / あなたが結果を気にしている（から）。 / それは～ということを意味します / あなたが真剣だという（ことを）。 / あなたは失敗したくありません。 / あなたはうまくいってほしいと思っています。 / 喜びなさい / あなたが悩んでいることを / そしてその気持ちを変えなさい / 何かポジティブなものに。 / それから、120％を出しなさい / あなたの目標を達成するために！",
   quizzes: [
-    { q: "Why does Shuzo say it is okay to be nervous?", options: ["Because it's funny", "Because you care about the results", "Because everyone is lazy", "Because tests are easy"], ans: "Because you care about the results" },
-    { q: "How much effort should you give according to Shuzo?", options: ["50%", "80%", "100%", "120%"], ans: "120%" }
+    { q: "Why does Shuzo say it is okay to be nervous?", options: ["Because it's funny", "Because you care about the results", "Because everyone is lazy", "Because tests are easy"], ans: "Because you care about the results", explanation: "Being nervous means you are serious about your success." },
+    { q: "How much effort should you give according to Shuzo?", options: ["50%", "80%", "100%", "120%"], ans: "120%", explanation: "He encourages us to give '120%' to achieve our goals." }
   ],
   vocab_quizzes: [
     { word: "nervous", meaning: "緊張して", options: ["緊張して", "怒って", "退屈して", "興奮して"] },
@@ -21,5 +21,7 @@ export const episode2: Episode = {
     { phrase: "care about ~", explanation: "「～を気にかける／大切に思う」。結果を重視している状態です。" },
     { phrase: "change A into B", explanation: "「AをBに変える」。ネガティブな感情をポジティブに変えるという文脈です。" }
   ],
-  dictation_items: ["often nervous during a test", "because you care about the", "means that you are serious", "change that feeling into something", "give one hundred twenty percent"]
+  dictation_items: ["often nervous during a test", "because you care about the", "means that you are serious", "change that feeling into something", "give one hundred twenty percent"],
+  overlappingTips: "【コツ】'give 120%!' の部分は力強く。自分を鼓舞するように発音しましょう。",
+  shadowingTips: "【コツ】'Presentation' や 'Serious' など、アクセントの位置に気をつけて追いかけましょう。"
 };
