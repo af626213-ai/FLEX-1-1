6b7695be1b0ea0926f785bcd1008ef770f31ce5b
af626213-ai <af.626213@gmail.com>
2026-04-24 18:34:35 +0900

    Create episode8.ts

---
+++ src/data/episodes/episode8.ts
@@ -0,0 +1,27 @@
+import { Episode } from '../types';
+
+export const episode8: Episode = {
+  id: 8,
+  title: "The Order of Priorities",
+  script: "Imagine what will happen if you put your sand into the jar first. After that, you will not be able to put all the rocks and pebbles into it. There will not be enough room for them. This is also true for your life. If you do too many unimportant things first, you will not have enough time to do the important things. To have a good life, you should pay attention to your rocks first. Pebbles come next, and then sand.",
+  slash_script: "Imagine / what will happen / if you put your sand / into the jar first. // After that, / you will not be able to put / all the rocks and pebbles / into it. // There will not be / enough room / for them. // This is also true / for your life. // If you do / too many unimportant things first, / you will not have enough time / to do the important things. // To have a good life, / you should pay attention / to your rocks first. // Pebbles come next, / and then sand.",
+  japanese_translation: "想像してみてください / 何が起こるかを / もしあなたが砂を入れたら / 最初に瓶の中に。 // その後では、 / あなたは入れることができなくなるでしょう / すべての石や小石を / その中に。 // ～はないでしょう / 十分なスペースが / それらのための。 // これはまた当てはまります / あなたの人生にも。 // もしあなたが～したら / あまりに多くの重要でないことを先に（したら）、 / あなたには十分な時間がなくなるでしょう / 重要なことをするための。 // 良い人生を送るために、 / あなたは注意を払うべきです / あなたの「石」に最初に。 // 小石が次に来て、 / そして砂です。",
+  quizzes: [
+    { q: "What happens if you put the sand in first?", options: ["The rocks fit better", "There is no room for the rocks", "The jar becomes beautiful", "The sand turns into rocks"], ans: "There is no room for the rocks", explanation: "Filling the life with small things first leaves no space for the important ones." },
+    { q: "What should you pay attention to first?", options: ["Your pebbles", "Your sand", "Your rocks", "Everything at once"], ans: "Your rocks", explanation: "The most important priority is the 'rocks'." }
+  ],
+  vocab_quizzes: [
+    { word: "Imagine", meaning: "想像する", options: ["説明する", "想像する", "練習する", "記憶する"] },
+    { word: "be able to", meaning: "〜することができる", options: ["〜するのをやめる", "〜し始める", "〜することができる", "〜してはいけない"] },
+    { word: "room", meaning: "余白・スペース", options: ["部屋", "家具", "余白・スペース", "建物"] },
+    { word: "unimportant", meaning: "重要でない", options: ["必要な", "重要でない", "有名な", "便利な"] },
+    { word: "pay attention to", meaning: "〜に注意を払う", options: ["〜に注意を払う", "〜を諦める", "〜を支払う", "〜に参加する"] }
+  ],
+  key_phrases: [
+    { phrase: "be able to ~", explanation: "「～することができる」。canと同じですが、助動詞の後（will be able to）などで活躍します。" },
+    { phrase: "pay attention to ~", explanation: "「～に注意を払う」。何かに集中したり、優先したりする時に使います。" }
+  ],
+  dictation_items: ["imagine what will happen if", "not be able to put", "not enough room for them", "too many unimportant things first", "pay attention to your rocks"],
+  overlappingTips: "【コツ】最後の結論 'Pebbles come next, and then sand.' は、教訓を言い聞かせるようにゆっくりと。",
+  shadowingTips: "【コツ】'Imagine' から始まる問いかけを、聞き手に届けるような気持ちで追いかけましょう。"
+};
