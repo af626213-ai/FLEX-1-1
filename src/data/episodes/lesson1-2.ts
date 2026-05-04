89f087eb0319d6ae0a74e215ea6762b55058471e
af626213-ai <af.626213@gmail.com>
2026-05-04 09:45:11 +0900

    Add lesson1-2 episode with script and quizzes

---
+++ src/data/episodes/lesson1-2.ts
@@ -0,0 +1,29 @@
+import { Episode } from '../types';
+
+export const lesson1_2: Episode = {
+  id: 2, // Lesson 1 Part 2 なので IDは 2
+  title: "It Is Good to Be Nervous!",
+  script: "“It is good to be nervous!” Are you often nervous during a test or a presentation? That is not a bad thing. You are nervous because you care about the results. That means that you are serious. You do not want to fail. You want to do well. Be glad that you are worried and change that feeling into something positive. Then, give 120% to achieve your goal! “When you have a hard time, smile.” Many people can’t smile when they are having a hard time. But that is the most important time to try to smile. A smile will have a good effect on you. It will also be good for the people around you. It is a way to bring cheer into people’s lives. Say loudly with a smile, “Next time, I’ll succeed!” If you say to yourself, “I can do it,” your heart and body will believe you.",
+  slash_script: "“It is good to be nervous!” // Are you often nervous / during a test / or a presentation? // That is not a bad thing. // You are nervous / because you care / about the results. // That means / that you are serious. // You do not want to fail. // You want to do well. // Be glad / that you are worried / and change that feeling / into something positive. // Then, / give 120% / to achieve your goal! // “When you have a hard time, / smile.” // Many people can’t smile / when they are having a hard time. // But that is / the most important time / to try to smile. // A smile will have / a good effect on you. // It will also be good / for the people around you. // It is a way / to bring cheer / into people’s lives. // Say loudly with a smile, / “Next time, I’ll succeed!” // If you say to yourself, / “I can do it,” / your heart and body / will believe you.",
+  japanese_translation: "「緊張するのは良いことです！」 // あなたはしばしば緊張しますか / テスト中や / プレゼンテーション中に？ // それは悪いことではありません。 // あなたが緊張するのは / 結果を気にしているからです。 // それは～を意味します / あなたが真剣であることを。 // あなたは失敗したくないのです。 // あなたは上手くいってほしいのです。 // 喜んでください / あなたが心配していることを / そしてその感情を変えてください / ポジティブなものへと。 // それから、 / 120％を出し切ってください / あなたの目標を達成するために！ // 「辛い時こそ、笑おう。」 // 多くの人は笑うことができません / 辛い時を過ごしている時には。 // しかし、それこそが / 最も重要な時なのです / 笑おうと努力するための。 // 笑顔は良い影響を与えてくれるでしょう / あなたに。 // それはまた、良いことでしょう / あなたの周りの人々にとっても。 // それは方法なのです / 元気をもたらすための / 人々の人生に。 // 笑顔で大きな声で言いましょう、 / 「次は成功する！」と。 // もしあなたが自分自身に言えば、 / 「自分ならできる」と / あなたの心と体は / あなたを信じてくれるでしょう。",
+  quizzes: [
+    { q: "Why does the author say it's good to be nervous?", options: ["Because you are lazy", "Because you care about the results", "Because you forgot your bag", "Because tests are easy"], ans: "Because you care about the results", explanation: "Being nervous means you are serious and care about the results." },
+    { q: "What should you do when you have a hard time?", options: ["Cry and give up", "Sleep all day", "Try to smile", "Get angry"], ans: "Try to smile", explanation: "The text says having a hard time is the most important time to try to smile." },
+    { q: "What happens if you say 'I can do it' to yourself?", options: ["You will become tired", "Nothing happens", "Your heart and body will believe you", "People will laugh"], ans: "Your heart and body will believe you", explanation: "The script says positive self-talk affects your heart and body." }
+  ],
+  vocab_quizzes: [
+    { word: "nervous", meaning: "緊張して", options: ["緊張して", "怒って", "退屈して", "幸せで"] },
+    { word: "result", meaning: "結果", options: ["原因", "結果", "方法", "理由"] },
+    { word: "achieve", meaning: "〜を達成する", options: ["〜を失う", "〜を達成する", "〜を避ける", "〜を探す"] },
+    { word: "effect", meaning: "影響・効果", options: ["努力", "影響・効果", "期待", "困難"] },
+    { word: "succeed", meaning: "成功する", options: ["失敗する", "成功する", "練習する", "去る"] }
+  ],
+  key_phrases: [
+    { phrase: "care about ~", explanation: "「～を気にする／～を大切に思う」。結果や他人のことを心配する時に使います。" },
+    { phrase: "change A into B", explanation: "「AをBに変える」。感情や形を変化させる時に便利な表現です。" },
+    { phrase: "say to oneself", explanation: "「心の中で思う／自分に言い聞かせる」。独り言や決意を表します。" }
+  ],
+  dictation_items: ["during a test or a presentation", "care about the results", "change that feeling into something", "bring cheer into people's lives", "your heart and body will believe"],
+  overlappingTips: "【コツ】'give 120%!' や 'I can do it!' など、カギカッコ内の台詞は自分自身に言い聞かせるように力強く発音しましょう。",
+  shadowingTips: "【コツ】'serious', 'important', 'believe' など、感情がこもる単語のアクセントをしっかり捉えて追いかけましょう。"
+};
